f3bd1b8e06aeeb3fa66d3b2ff1032d02ba401147
Will Sewell <me@willsewell.name>
2014-10-12 14:06:38 -0500

    Created a representation of planets and the players pod, along with a simple way of rendering them.

---
+++ Main.elm
@@ -0,0 +1,31 @@
+import Keyboard
+import Window
+import Math.Vector2 as V2
+
+type Pod = { pos:V2.Vec2, vel:V2.Vec2 }
+type Planet = { pos:V2.Vec2, mass:Float }
+type Game = { pod:Pod, planets:[Planet] }
+
+defaultGame : Game
+defaultGame =
+  { pod     = { pos=V2.vec2 0 0, vel=V2.vec2 0 0 }
+  , planets = [ { pos=V2.vec2 50 50,   mass=5 }
+              , { pos=V2.vec2 -100 0, mass=10 }
+              , { pos=V2.vec2 150 200,  mass=15 }] }
+
+step : (Float,{ x:Int, y:Int }) -> Game -> Game
+step (t,dir) {pod,planets} = {pod=pod,planets=planets}
+              
+render : (Int,Int) -> Game -> Element
+render (w',h') {pod,planets} =
+  let (w,h) = (toFloat w', toFloat h')
+  in collage w' h'
+    ((circle 10 |> outlined (solid (rgb 50 150 200)) |> move (V2.getX pod.pos, V2.getY pod.pos))
+    :: map (\planet -> circle planet.mass |> outlined (solid (rgb 200 150 50)) |> move (V2.getX planet.pos, V2.getY planet.pos)) planets)
+
+input : Signal (Float,{ x:Int, y:Int })
+input = let delta = lift (\t -> t/20) (fps 24)
+        in sampleOn delta (lift2 (,) delta Keyboard.arrows)
+
+main = lift2 render Window.dimensions (foldp step defaultGame input)
+  
