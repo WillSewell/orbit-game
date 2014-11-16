ebb113e3bef5f9d78c7ac179773ead88b3341e42
Will Sewell <me@willsewell.name>
2014-11-15 21:52:31 -0500

    Refactored the render code to reduce line lnegth and make it more readable.

--- Main.elm
+++ Main.elm
@@ -9,9 +9,9 @@ type Game = { pod:Pod, planets:[Planet] }
 defaultGame : Game
 defaultGame =
   { pod     = { pos=V2.vec2 0 0, vel=V2.vec2 0 0 }
-  , planets = [ { pos=V2.vec2 50 50,   mass=5 }
+  , planets = [ { pos=V2.vec2 50 50, mass=5 }
               , { pos=V2.vec2 -100 0, mass=10 }
-              , { pos=V2.vec2 150 200,  mass=15 }] }
+              , { pos=V2.vec2 150 200, mass=15 }] }
 
 step : (Float,{ x:Int, y:Int }) -> Game -> Game
 step (t,dir) {pod,planets} = {pod=pod,planets=planets}
@@ -20,8 +20,13 @@ render : (Int,Int) -> Game -> Element
 render (w',h') {pod,planets} =
   let (w,h) = (toFloat w', toFloat h')
   in collage w' h'
-    ((circle 10 |> outlined (solid (rgb 50 150 200)) |> move (V2.getX pod.pos, V2.getY pod.pos))
-    :: map (\planet -> circle planet.mass |> outlined (solid (rgb 200 150 50)) |> move (V2.getX planet.pos, V2.getY planet.pos)) planets)
+    ((circle 10 |> outlined (solid (rgb 50 150 200)) 
+                |> move (V2.getX pod.pos, V2.getY pod.pos))
+     :: map renderPlanet planets)
+
+renderPlanet : Planet -> Form
+renderPlanet p = circle p.mass |> outlined (solid (rgb 200 150 50))
+                               |> move (V2.getX p.pos, V2.getY p.pos)
 
 input : Signal (Float,{ x:Int, y:Int })
 input = let delta = lift (\t -> t/20) (fps 24)
