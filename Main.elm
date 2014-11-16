afbd59bbece8d4aa1c13cb997dec3c525c1039b1
Will Sewell <me@willsewell.name>
2014-11-16 15:30:19 -0500

    Split out pod rendering code into a new function in a similar way to the planets.

--- Main.elm
+++ Main.elm
@@ -31,10 +31,11 @@ gravityPull planets p = foldl (\planet pod -> { p | vel <- p.vel }) p planets {-
 render : (Int,Int) -> Game -> Element
 render (w',h') {pod,planets} =
   let (w,h) = (toFloat w', toFloat h')
-  in collage w' h'
-    ((square 10 |> outlined (solid (rgb 50 150 200)) 
-                |> move (V2.getX pod.pos, V2.getY pod.pos))
-     :: map renderPlanet planets)
+  in collage w' h' (renderPod pod :: map renderPlanet planets)
+
+renderPod : Pod -> Form
+renderPod p = square 10 |> outlined (solid (rgb 50 150 200)) 
+                        |> move (V2.getX p.pos, V2.getY p.pos)
 
 renderPlanet : Planet -> Form
 renderPlanet p = circle p.mass |> outlined (solid (rgb 200 150 50))
