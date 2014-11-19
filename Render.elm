ccc68236f89749c5d70f48799ca620766fff0929
Will Sewell <me@willsewell.name>
2014-11-19 22:14:49 +0000

    Rearranged rendering order so that the explosion is rendered last.

--- Render.elm
+++ Render.elm
@@ -8,9 +8,9 @@ import Planet (Planet)
 render : (Int,Int) -> Game -> Element
 render (w',h') {pod,planets,explosionSize} =
   let (w,h) = (toFloat w', toFloat h')
-  in collage w' h' (renderBg (w,h) :: renderPod pod
-                                   :: renderExplosion pod explosionSize
-                                   :: map renderPlanet planets)
+  in collage w' h' ([renderBg (w,h), renderPod pod]
+                    ++ map renderPlanet planets
+                    ++ [renderExplosion pod explosionSize])
 
 renderBg : (Float,Float) -> Form
 renderBg (w,h) = rect w h |> filled black
