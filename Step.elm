d6861b2b6d5535e7aeafdb126e79702157d76884
Will Sewell <me@willsewell.name>
2014-11-22 15:13:25 +0000

    The previous commit was buggy, and I simplified it by having the logic in a let statement instead. Also simplified the isCollided code by only partially applying the parameters.

--- Step.elm
+++ Step.elm
@@ -8,11 +8,11 @@ import Planet (Planet)
 
 step : (Float,{ x:Int, y:Int }) -> Game -> Game
 step (t,dir) g = 
-  let collided = isCollided g.planets g.pod
+  let collided = isCollided g.pod g.planets
+      isExploding = (g.explosionSize > 0 || collided) && g.explosionSize < 50
   in { g | pod <- updatePod g dir t
          , state <- if collided then Ended else Running
-         , explosionSize <- g.explosionSize + if isExploding g.explosionSize
-                                              then 15 else 0 }
+         , explosionSize <- g.explosionSize + if isExploding then 15 else 0 }
 
 updatePod : Game -> { x:Int, y:Int } -> Float -> Pod
 updatePod g dir t = case g.state of
@@ -22,13 +22,10 @@ updatePod g dir t = case g.state of
                    |> updateBoostDir dir
   _ -> g.pod
 
-isCollided : [Planet] -> Pod -> Bool
-isCollided planets pod = any (\planet -> V2.distance planet.pos pod.pos < planet.mass + 5) planets
+isCollided : Pod -> [Planet] -> Bool
+isCollided pod = any (\planet -> V2.distance planet.pos pod.pos < planet.mass + 5)
 
 updateBoostDir : { x:Int, y:Int } -> Pod -> Pod
 updateBoostDir dir pod = { pod |
   boostDir <- (if dir.x == 1 then [L] else if dir.x == -1 then [R] else [])
               ++ (if dir.y == 1 then [U] else if dir.y == -1 then [D] else []) }
-
-isExploding : Float -> Bool
-isExploding explosionSize = (g.explosionSize > 0 || collided) && g.explosionSize < 50
