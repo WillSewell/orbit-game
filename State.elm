7f68f341dafee919464370368947e9b71d97bedd
Will Sewell <me@willsewell.name>
2014-11-26 21:06:02 +0000

    Documented State.elm.

--- State.elm
+++ State.elm
@@ -1,17 +1,24 @@
+{-| Module containing types that represent the current state of the game.
+Also contains the defaultGame (the start state). -}
 module State where
 
 import Math.Vector2 as V2
 import Pod (Pod)
 import Planet (Planet)
 
+{-| Represents the current state of the game. -}
 data Game = Game { pod:Pod
                  , planets:[Planet]
                  , state:GameState
                  , explosionSize:Float
+                   {- future states of the pod provided direction is not changed 
+                      used to compute the trejectory. -}
                  , futureStates:[Pod] }
-          
+
+{-| Whether the game is running, or in another state. -}
 data GameState = Running | Ended
 
+{-| The configuration of the initial game state. -}
 defaultGame : Game
 defaultGame = Game
   { pod = { pos=V2.vec2 0 0
