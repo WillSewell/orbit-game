d19eb9e8ff23a04dd82761a2bfa2052558919376
Will Sewell <me@willsewell.name>
2014-11-26 21:18:43 +0000

    Documented Config.elm

--- Config.elm
+++ Config.elm
@@ -1,7 +1,10 @@
+{-| Module for storing game constants. -}
 module Config where
 
+{-| Record of all game constants. -}
 type Config = { boostPwrFactor:Float, gravPwrFactor:Float }
 
+{-| Where the constants are set, and how the game retrieves them. -}
 config : Config
 config = { boostPwrFactor = 0.02
          , gravPwrFactor = 0.0005 }
