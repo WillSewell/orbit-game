e85cc9253b930b65d1c91d41b4f3f2b8f51acf6b
Will Sewell <me@willsewell.name>
2014-11-26 20:59:39 +0000

    Documented Main.elm.

--- Main.elm
+++ Main.elm
@@ -1,3 +1,4 @@
+{-| Main module that folds the step function over the input, starting from the defaultGame. -}
 module Main where
 
 import Keyboard
@@ -6,8 +7,11 @@ import State (defaultGame)
 import Render (render)
 import Step (step)
 
+{-| input is a signal consisting of the pressed arrow keys, and the delta
+- the time between frames. -}
 input : Signal (Float,{ x:Int, y:Int })
 input = let delta = lift (\t -> t/20) (fps 24)
         in sampleOn delta (lift2 (,) delta Keyboard.arrows)
 
+{-| Run the game - see module doc. -}
 main = lift2 render Window.dimensions (foldp step defaultGame input)
