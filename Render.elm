e036bc91be2bab09c24b2ca7fd2f7c791c445d39
Will Sewell <me@willsewell.name>
2014-11-26 21:40:31 +0000

    Documented Render.elm

--- Render.elm
+++ Render.elm
@@ -1,3 +1,4 @@
+{-| Module that takes a game state, and renders it. -}
 module Render where
 
 import Math.Vector2 as V2
@@ -6,25 +7,35 @@ import Pod (Pod, BoostDir(..))
 import Planet (Planet)
 import Util (vec2Pair)
 
+{-| The main render function that renders the state by creating a
+collage in the window dimensions. -}
 render : (Int,Int) -> Game -> Element
 render (w',h') (Game { pod, planets, explosionSize, futureStates }) =
   let (w,h) = (toFloat w', toFloat h')
+     {- render each component with a helper function and add it to the list -}
   in collage w' (h' - 20) ([renderBg (w,h), renderPod pod, renderTrejactory futureStates]
                            ++ map renderPlanet planets
                            ++ map (renderBoost pod) pod.boostDir
                            ++ [renderExplosion pod explosionSize])
+     {- render game stats -}
      `below` asText ("Fuel: " ++ show pod.fuel)
 
+{-| Create a black background. -}
 renderBg : (Float,Float) -> Form
 renderBg (w,h) = rect w h |> filled black
 
+{-| Render the pod. -}
 renderPod : Pod -> Form
 renderPod pod = square 10 |> filled blue
                           |> move (vec2Pair pod.pos)
 
+{-| Create a dashed line from the future pod states that is its trejectory. -}
 renderTrejactory : [Pod] -> Form
 renderTrejactory pods = path (map (\pod -> (vec2Pair pod.pos)) pods) |> traced (dashed red)
 
+{-| Render a boost a boost based on the direction.
+This function should be called multiple times,
+because more than one boost could fire at once. -}
 renderBoost : Pod -> BoostDir -> Form
 renderBoost pod boostDir = 
   let (oldX, oldY) = vec2Pair pod.pos
@@ -39,10 +50,12 @@ renderBoost pod boostDir =
     |> move newPos
     |> rotate (degrees rotDegrees)
 
+{-| Render a planet. -}
 renderPlanet : Planet -> Form
 renderPlanet planet = circle planet.mass |> filled brown
                                          |> move (vec2Pair planet.pos)
 
+{-| Render an explosion as an expanding red ball. -}
 renderExplosion : Pod -> Float -> Form
 renderExplosion pod exploSize = circle exploSize 
   |> gradient (radial (0,0) 10 (0,10) (exploSize) [(0, rgb 252 75 65), (1, rgba 228 199 0 0)])
