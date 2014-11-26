4d1aed9eb832bcf5dc4a62f014135695f3b5b1c0
Will Sewell <me@willsewell.name>
2014-11-26 21:28:04 +0000

    Documented Physics.elm

--- Physics.elm
+++ Physics.elm
@@ -1,3 +1,4 @@
+{-| Module with functions dealing with physics updates. -}
 module Physics where
 
 import Math.Vector2 as V2
@@ -5,17 +6,22 @@ import Pod (Pod)
 import Planet (Planet)
 import Config (config)
 
+{-| Update the position of the pod based on velocity and position. -}
 physics : Float -> Pod -> Pod
 physics t p = { p | pos <- p.pos `V2.add` V2.scale t p.vel }
 
+{-| Update the velocity in the direction the user is pressing. -}
 boost : { x:Int, y:Int } -> Pod -> Pod
 boost { x, y } p = 
   let scaleDir x = toFloat x * config.boostPwrFactor
   in { p | vel <- p.vel `V2.add` V2.vec2 (scaleDir x) (scaleDir y) }
 
+{-| Loop through the planets, and have them pull the pod closer. -}
 gravityPullAll : [Planet] -> Pod -> Pod
 gravityPullAll planets pod = foldl (gravityPull pod) pod planets
 
+{-| Pull the pod towards a planet (update velocity in that direction.
+Strength of pull is based on the distance, and the mass of the planet. -}
 gravityPull : Pod -> Planet -> Pod -> Pod
 gravityPull oldPod planet pod =
   let distance = V2.distance pod.pos planet.pos in
