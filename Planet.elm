f683c45673e467c4d95fbce8c9f49644357af835
Will Sewell <me@willsewell.name>
2014-11-26 21:15:51 +0000

    Documented Pod and Planet modules (very similar).

--- Planet.elm
+++ Planet.elm
@@ -1,6 +1,9 @@
+{-| Module for holding types to represent planets,
+as well as utility functions for interacting with it. -}
 module Planet where
 
 import Util (Positioned)
 import Math.Vector2 as V2
 
+{-| Represents a planet. -}
 type Planet = Positioned { mass:Float }
--- Pod.elm
+++ Pod.elm
@@ -1,11 +1,15 @@
+{-| Module for holding types to represent the pod,
+as well as utility functions for interacting with it. -}
 module Pod where
 
 import Util (Positioned)
 import Math.Vector2 as V2
 
+{-| Represents the pod. -}
 type Pod = Positioned { vel:V2.Vec2
                       , collided:Bool
                       , boostDir:[BoostDir]
                       , fuel:Int }
 
+{-| Represents the current boost direction. -}
 data BoostDir = U | D | L | R
