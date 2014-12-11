0fefcd76dc8aed8548ad8dad59954924fbd643de
Will Sewell <me@willsewell.name>
2014-12-11 22:50:17 +0000

    Updated config to store locations of level files.

--- Config.elm
+++ Config.elm
@@ -5,14 +5,17 @@ import State (Game(..), GameState(..))
 import Math.Vector2 as V2
 
 {-| Record of all game constants. -}
-type Config = { boostPwrFactor:Float, gravPwrFactor:Float }
+type Config = { boostPwrFactor : Float
+              , gravPwrFactor : Float
+              , levels : [String] }
 
 {-| Where the constants are set, and how the game retrieves them. -}
 config : Config
 config = { boostPwrFactor = 0.02
-         , gravPwrFactor = 0.0001 }
+         , gravPwrFactor = 0.0001
+         , levels = [ "worlds/1.json" ] }
 
-{-| The configuration of the initial game state. -}
+{-| Dummy configuration used before any game file is loaded. -}
 defaultGame : Game
 defaultGame = Game
   { pod = { pos=V2.vec2 0 0
